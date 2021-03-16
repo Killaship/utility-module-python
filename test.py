ee34d3c7ec13d7df1f37bd9a57c628eb82cee3dc
Killaship <69988679+Killaship@users.noreply.github.com>
2021-03-16 15:13:31 -0400

    add a comment

--- test.py
+++ test.py
@@ -1,6 +1,9 @@
 from utilities import loadingbar
 from utilities import test
 
+
+# the test module was just me messing around, but this is an example of all the modules so far. documentation is in the actual utilities module
+
 loadingbar(20, 0.2, "|||")
 
 print(test())
